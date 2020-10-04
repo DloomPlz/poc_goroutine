9aaa6f191e3b2ff98ebe7d6613053a6061c6cbbf
Pulsz <pulsz@pulszs-macbook-air.home>
2020-10-04 15:21:07 +0200

    first

---
+++ cmd/test.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// testCmd represents the test command
+var testCmd = &cobra.Command{
+	Use:   "test",
+	Short: "test",
+	Long:  `test`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("test called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(testCmd)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "poc/cmd"
+
+func main() {
+	cmd.Execute()
+}
