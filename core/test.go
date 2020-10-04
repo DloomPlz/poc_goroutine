f5ea74a06f88dc4f5b578ac441dc98c4ccd3704e
Pulsz <pulsz@pulszs-macbook-air.home>
2020-10-04 15:29:48 +0200

    goroutine example

--- core/test.go
+++ core/test.go
@@ -1,7 +1,21 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func Test() {
+	wg := new(sync.WaitGroup)
 	fmt.Println("core.Test")
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			time.Sleep(1 * time.Second)
+			fmt.Printf("goroutine running : %d \n", i)
+		}(i)
+	}
+	wg.Wait()
 }
