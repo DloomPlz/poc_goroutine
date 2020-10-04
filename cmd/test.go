8fcf89e857061053565c75d2c0f401ac7f3cdfdd
Pulsz <pulsz@pulszs-macbook-air.home>
2020-10-04 15:23:23 +0200

    core

--- cmd/test.go
+++ cmd/test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"poc/core"
 
 	"github.com/spf13/cobra"
 )
@@ -13,6 +14,7 @@ var testCmd = &cobra.Command{
 	Long:  `test`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("test called")
+		core.Test()
 	},
 }
 
---
+++ core/test.go
@@ -0,0 +1,7 @@
+package core
+
+import "fmt"
+
+func Test() {
+	fmt.Println("core.Test")
+}
