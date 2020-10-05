3155e2e509449ef81960e0034a4ff3b1a9caeb67
PulsZ <neostan49@gmail.com>
2020-10-05 20:15:24 +0200

    Add context

--- core/test.go
+++ core/test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -8,14 +9,22 @@ import (
 
 func Test() {
 	wg := new(sync.WaitGroup)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	fmt.Println("core.Test")
 	for i := 1; i <= 10; i++ {
 		wg.Add(1)
-		go func(i int) {
+		go func(ctx context.Context, i int) {
 			defer wg.Done()
-			time.Sleep(1 * time.Second)
-			fmt.Printf("goroutine running : %d \n", i)
-		}(i)
+			select {
+			case <-ctx.Done():
+				fmt.Println("Done:", ctx.Err())
+				return
+			case r := <-time.After(2 * time.Second):
+				fmt.Println("", r)
+				fmt.Printf("goroutine running : %d \n", i)
+			}
+		}(ctx, i)
 	}
 	wg.Wait()
+	cancel()
 }
